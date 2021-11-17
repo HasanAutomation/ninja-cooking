285d563f68849f5ef855d1f3fd6a43e6c7cbb533
Hasan Ali <hasan.ali@lacritz.com>
2021-11-17 20:59:23 +0530

    Added Search Component

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Recipes from '../../components/recipes/Recipes';
+import { useFetch } from '../../hooks';
 
 export default function Search() {
-  return <div>Search</div>;
+  const queryString = useLocation().search;
+
+  const query = queryString.split('?')[1];
+
+  const url = `http://localhost:3000/recipes?${query}`;
+
+  const { loading, error, data } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className='page-title'>Recipes including {query.split('=')[1]}</h2>
+      {loading && <p>Loading....</p>}
+      {error && <p className='error'>{error}</p>}
+      <Recipes recipes={data} />
+    </div>
+  );
 }
